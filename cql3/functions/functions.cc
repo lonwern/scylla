1446ffd0284fc915fd2aba638b2a03bcffb08232
lonwern <lonwern@gmail.com>
2019-01-16 10:56:30 +0800

    cql3/functions: Add functions for bool type

--- cql3/functions/functions.cc
+++ cql3/functions/functions.cc
@@ -123,6 +123,10 @@ functions::init() {
     declare(aggregate_fcts::make_max_function<utils::UUID>());
     declare(aggregate_fcts::make_min_function<utils::UUID>());
 
+    declare(aggregate_fcts::make_count_function<bool>());
+    declare(aggregate_fcts::make_max_function<bool>());
+    declare(aggregate_fcts::make_min_function<bool>());
+
     //FIXME:
     //declare(aggregate_fcts::make_count_function<bytes>());
     //declare(aggregate_fcts::make_max_function<bytes>());
